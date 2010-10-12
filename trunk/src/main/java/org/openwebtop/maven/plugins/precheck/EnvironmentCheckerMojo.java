c9ea769e1472b6290191becf65ce8866058fec00
dotoli21 <dotoli21@604305ca-344e-ce39-e1a3-05c51f4f5f6d>
2010-10-12 10:21:52 +0000

    enviroment checker 추가개발

--- trunk/src/main/java/org/openwebtop/maven/plugins/precheck/EnvironmentCheckerMojo.java
+++ trunk/src/main/java/org/openwebtop/maven/plugins/precheck/EnvironmentCheckerMojo.java
@@ -1,9 +1,19 @@
 package org.openwebtop.maven.plugins.precheck;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang.ArrayUtils;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.codehaus.plexus.util.DirectoryScanner;
 import org.openwebtop.maven.plugins.precheck.environment.Environment;
+import org.openwebtop.maven.plugins.precheck.environment.EnvironmentChecker;
+import org.openwebtop.maven.plugins.precheck.environment.EnvironmentFiles;
+import org.openwebtop.maven.plugins.precheck.environment.model.EnvironmentError;
 
 /**
  * 
@@ -23,12 +33,6 @@ public class EnvironmentCheckerMojo extends AbstractPrecheckMojo {
 	 */
 	private Environment[] environments;
 
-	private DirectoryScanner directoryScanner;
-
-	public EnvironmentCheckerMojo() {
-		directoryScanner = new DirectoryScanner();
-	}
-
 	@Override
 	public String getGoalName() {
 		return GOAL_NAME;
@@ -36,24 +40,65 @@ public class EnvironmentCheckerMojo extends AbstractPrecheckMojo {
 
 	@Override
 	public void execute() throws MojoExecutionException, MojoFailureException {
-		directoryScanner.setBasedir(environments[0].getCheckSource().getBasedir());
-		directoryScanner.setIncludes(environments[0].getCheckSource().getIncludes());
-		directoryScanner.setExcludes(environments[0].getCheckSource().getExcludes());
+		printLog("----- Start to check environment -----");
+
+		if (skip) {
+			printLog("----- Skip check environment -----");
+			return;
+		}
+
+		if (ArrayUtils.isEmpty(environments)) {
+			printLog("There is no configuration for checking environment. skipping...");
+
+			return;
+		}
+
+		final List<EnvironmentError> environmentErrors = new ArrayList<EnvironmentError>();
+
+		for (Environment environment : environments) {
+			final EnvironmentChecker environmentChecker = new EnvironmentChecker();
+			environmentChecker.setExtractRegExp(environment.getExtractRegExp());
+			environmentChecker.setCheckTargetFiles(getFilelist(environment.getCheckTarget()));
+			environmentChecker.setCheckSourceFiles(getFilelist(environment.getCheckSource()));
+
+			try {
+				environmentErrors.addAll(environmentChecker.check());
+			} catch (IOException e) {
+				throw new MojoFailureException("Cannot scan some files!");
+			}
+		}
+
+		if (CollectionUtils.isNotEmpty(environmentErrors)) {
+			for (EnvironmentError environmentError : environmentErrors) {
+				printLog(environmentError.toString());
+			}
+
+			throw new MojoFailureException(String.format("%d files have been detected!", environmentErrors.size()));
+		}
+
+		printLog("----- There is no prohibit text -----");
+	}
+
+	private File[] getFilelist(EnvironmentFiles environmentFiles) {
+		final DirectoryScanner directoryScanner = new DirectoryScanner();
+		directoryScanner.setBasedir(environmentFiles.getBasedir());
+		directoryScanner.setIncludes(environmentFiles.getIncludes());
+		directoryScanner.setExcludes(environmentFiles.getExcludes());
 		directoryScanner.scan();
 
-		final String[] filenames = directoryScanner.getIncludedFiles();
+		final String[] filelist = directoryScanner.getIncludedFiles();
 
-		for (String filename : filenames) {
-			System.out.println(filename);
+		final List<File> files = new ArrayList<File>();
+
+		for (String filename : filelist) {
+			files.add(new File(environmentFiles.getBasedir() + File.separator + filename));
 		}
+
+		return files.toArray(new File[0]);
 	}
 
 	public void setEnvironments(Environment[] environments) {
 		this.environments = environments;
-	}
-
-	public void setDirectoryScanner(DirectoryScanner directoryScanner) {
-		this.directoryScanner = directoryScanner;
 	}
 
 }
--- trunk/src/main/java/org/openwebtop/maven/plugins/precheck/environment/model/EnvironmentError.java
+++ trunk/src/main/java/org/openwebtop/maven/plugins/precheck/environment/model/EnvironmentError.java
@@ -1,5 +1,48 @@
 package org.openwebtop.maven.plugins.precheck.environment.model;
 
+import java.io.File;
+
 public class EnvironmentError {
+	private File file;
+	private int lineNumber;
+	private String line;
+	private String detectedString;
+
+	public File getFile() {
+		return file;
+	}
+
+	public void setFile(File file) {
+		this.file = file;
+	}
+
+	public int getLineNumber() {
+		return lineNumber;
+	}
+
+	public void setLineNumber(int lineNumber) {
+		this.lineNumber = lineNumber;
+	}
+
+	public String getLine() {
+		return line;
+	}
+
+	public void setLine(String line) {
+		this.line = line;
+	}
+
+	public String getDetectedString() {
+		return detectedString;
+	}
+
+	public void setDetectedString(String detectedString) {
+		this.detectedString = detectedString;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("File=%s, Line=%s, Lines=%d, DetectedString=%s", file.getPath(), line, lineNumber, detectedString);
+	}
 
 }
